4ce0e6c7207e72c0231831d114d95972ff4d1fd9
Devin Sather <devin.sather@outlook.com>
2022-06-19 10:12:32 -0700

    Homescreen looks decent

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,21 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+import Header from './components/Header.js'
+import Footer from './components/Footer.js'
+import HomeScreen from './screens/HomeScreen'
 const App = () => {
   return (
     <>
-      <h1> Welcome To ProShop </h1>
+      <Header />
+      <main className="py-3">
+          <Container>
+            <HomeScreen />
+          </Container>
+      </main>
+      <Footer />
     </>
   );
 }
+
 
 export default App;
